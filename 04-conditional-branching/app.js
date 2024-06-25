6d4e06e0c550cf04ebe31d76ea14ca50db48e017
bernard konan <devwithbernard@gmail.com>
2024-06-25 09:53:43 +0200

    Conditional statement: If and Else branching, Ternary operator

--- 04-conditional-branching/app.js
+++ 04-conditional-branching/app.js
@@ -1,46 +1,61 @@
 // If Statement
-let age = prompt('What age Jamie Lannister are ?');
+let age = prompt("What age Jamie Lannister are ?");
 
 if (Number(age) === 43) {
-    alert("You're right");
-}else{
-    alert('😥😥 Wrong answer.');
+  alert("You're right");
+} else {
+  alert("😥😥 Wrong answer.");
 }
 
 // Boolean Conversion
 if (!0) {
-    alert('0 is falsy');
+  alert("0 is falsy");
 }
 if (!"") {
-    alert('Empty String is falsy');
+  alert("Empty String is falsy");
 }
 if (!undefined) {
-    alert("'Undefined' is falsy");
+  alert("'Undefined' is falsy");
 }
 if (!NaN) {
-    alert("NaN is falsy");
+  alert("NaN is falsy");
 }
 
 // Boolean Evaluation
-const currentUser = 'Jane';
-const isConnected = currentUser.toLowerCase() === 'jane';
+const currentUser = "Jane";
+const isConnected = currentUser.toLowerCase() === "jane";
 
 if (isConnected) {
-    alert('Jane is connected');
-}else {
-    alert("😒😒 You're not connected./nSign up please");
+  alert("Jane is connected");
+} else {
+  alert("😒😒 You're not connected./nSign up please");
 }
 
 // Else If Statement
-let age = parseInt(prompt('What age do you have?'));
+age = parseInt(prompt("What age do you have?"));
 if (age < 0) {
-    alert('Impossible');
-}else if (age < 12) {
-    alert('Too young');
+  alert("Impossible");
+} else if (age < 12) {
+  alert("Too young");
 } else if (age < 18) {
-    alert('Not allow Yet');
-}else if (age < 50) {
-    alert('Allow to watch');
-}else{
-    alert("You're an older man.");
+  alert("Not allow Yet");
+} else if (age < 50) {
+  alert("Allow to watch");
+} else {
+  alert("You're an older man.");
+}
+
+// Ternary operator
+let accessAllowed;
+age = prompt("How old are you?", "");
+accessAllowed = !isNaN(age) && Number(age) >= 18 ? true : false;
+// can be simplified to accessAllowed = !isNaN(age) && Number(age) > 18
+
+if (accessAllowed) {
+  alert("Welcome to this website");
+} else {
+  const div = document.createElement("div");
+  div.classList.add("alert-danger");
+  div.innerText = "😢😢\nYou're not authorized to access to this website";
+  document.body.appendChild(div);
 }
